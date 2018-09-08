700bd3c1ff2d0084a7590449c782a17f18e73dc5
Bleve <wsbaihekong@gmail.com>
2018-09-08 20:37:29 +1000

    9.8

---
+++ Assignment2/Assignment2/Assignment2/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2
+{
+    public static class Constants
+    {
+        public static string Username = "Deakin";
+        public static string Password = "194461";
+    }
+}
